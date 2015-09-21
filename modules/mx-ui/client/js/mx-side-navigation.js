68b5553de970810cb71e8cabf44f325faa494779
Nizul Zaim <shimizukazunari93@gmail.com>
2015-09-21 09:33:26 +0800

    Fix Side Navigation Submenu
    Navigation Submenu will call second time although have been call before

--- modules/mx-ui/client/js/mx-side-navigation.js
+++ modules/mx-ui/client/js/mx-side-navigation.js
@@ -35,47 +35,49 @@ $.fn.mxSideNavigation = function(args) {
 		$index -= 1;
 
 		if($(this).hasClass("submenu")){
-			var $next = $(this).next();
-			var $nextHeight = $next.outerHeight();
+            if(!$(this).isMeteorOnce("side-navigation-submenu") ){
+                var $next = $(this).next();
+    			var $nextHeight = $next.outerHeight();
 
-			$next.css({
-				"margin-top": "-"+ $nextHeight + "px",
-				"transition": "margin-top 0.3s ease-in-out",
-				"z-index" : $index
-			});
-			$index -= 1;
+    			$next.css({
+    				"margin-top": "-"+ $nextHeight + "px",
+    				"transition": "margin-top 0.3s ease-in-out",
+    				"z-index" : $index
+    			});
+    			$index -= 1;
 
-			$(this).prepend('<div class="chevron"><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg></div>');
-			var $chevron = $(this).find(".chevron");
+    			$(this).prepend('<div class="chevron"><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg></div>');
+    			var $chevron = $(this).find(".chevron");
 
-			if ($next.hasClass("show")) {
-				$next.css({
-					"margin-top":0,
-				});
-				$chevron.addClass("show");
-			}
+    			if ($next.hasClass("show")) {
+    				$next.css({
+    					"margin-top":0,
+    				});
+    				$chevron.addClass("show");
+    			}
 
-			$(this).on("click", function() {
-
-
-				if ($next.hasClass("show")) {
-					$next.css({
-						"margin-top": "-"+ $nextHeight + "px"
-					});
-					$next.removeClass("show");
-				} else {
-					$next.css({
-						"margin-top": 0
-					});
-					$next.addClass("show");
-				}
-				var $activeNav = $target.find(".nav-list.active");
-				$activeNav.removeClass("active");
-				$(this).addClass("active");
-				$chevron.toggleClass("show");
+    			$(this).on("click", function() {
 
 
-			});
+    				if ($next.hasClass("show")) {
+    					$next.css({
+    						"margin-top": "-"+ $nextHeight + "px"
+    					});
+    					$next.removeClass("show");
+    				} else {
+    					$next.css({
+    						"margin-top": 0
+    					});
+    					$next.addClass("show");
+    				}
+    				var $activeNav = $target.find(".nav-list.active");
+    				$activeNav.removeClass("active");
+    				$(this).addClass("active");
+    				$chevron.toggleClass("show");
+
+
+    			});
+            }
 		} else {
 			$(this).on("click", function() {
 				var $activeNav = $target.find(".nav-list.active");
