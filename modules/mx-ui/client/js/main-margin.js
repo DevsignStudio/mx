553e05d9726ce2002c846f89d03bfebe59b024c6
Nizul Zaim <shimizukazunari93@gmail.com>
2015-09-26 16:47:42 +0800

    Update page container and scrollbar

--- modules/mx-ui/client/js/main-margin.js
+++ modules/mx-ui/client/js/main-margin.js
@@ -1,48 +1,15 @@
 Mx.doAfterRouterLoad(function(){
-    $(".toolbar.above, .toolbar.cover").each(function() {
+    $(".toolbar").each(function() {
     	var $height = $(this).outerHeight();
     	var $margin = parseInt($(this).next().css("marginTop"));
+        var marginBefore = $(this).next().data("margin-top-before") === undefined ?
+                            0 : $(this).next().data("margin-before");
 
         $(this).next().mxMargin({
-            "margin-top" : $height + $margin
-        });
-        var parentHeight = $(this).parent().outerHeight();
-        $(this).next().css({
-        	height: parentHeight - $height + $margin
+            "margin-top" : $height + $margin - parseInt(marginBefore)
         });
 
-
+        $(this).next().data("margin-top-before",
+                            $height + $margin - parseInt(marginBefore));
     });
-
-    var deviceAgent = navigator.userAgent.toLowerCase();
-
-    var isTouchDevice = Modernizr.touch ||
-    (deviceAgent.match(/(iphone|ipod|ipad)/) ||
-    deviceAgent.match(/(android)/)  ||
-    deviceAgent.match(/(iemobile)/) ||
-    deviceAgent.match(/iphone/i) ||
-    deviceAgent.match(/ipad/i) ||
-    deviceAgent.match(/ipod/i) ||
-    deviceAgent.match(/blackberry/i) ||
-    deviceAgent.match(/bada/i));
-
-    if (isTouchDevice) {
-        $pageCon = $(".page-container");
-
-        $pageCon.each(function() {
-            $pageCon.css("overflow", "auto");
-        })
-    } else {
-        $pageCon = $(".page-container");
-
-        $pageCon.each(function() {
-            $(this).removeClass("mCustomScrollbar _mCS_1 mCS-autoHide");
-            $(this).mCustomScrollbar({
-                axis:"y",
-                theme:"minimal-dark",
-                scrollInertia: 300
-            });
-        });
-    }
-
 });
