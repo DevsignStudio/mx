310a263c200d585f13c043c0b47b6ac17d5d45eb
Nizul Zaim <shimizukazunari93@gmail.com>
2015-09-28 01:08:39 +0800

    Partially Fix glitch in Toolbar Fix After

--- modules/mx-ui/client/js/mx-toolbar.js
+++ modules/mx-ui/client/js/mx-toolbar.js
@@ -12,26 +12,69 @@ $.fn.mxFixAfterToolbar = function(args) {
     var breakPoint = height + offsetTop;
     var $position = 0;
     var positionDifference = 0;
+    var stickToTop = false;
+    var count = 0;
 
     $(window).on("scroll", function() {
-        if ($(document).scrollTop() > breakPoint) {
+        scrollTop = $(document).scrollTop();
+        // console.log(scrollTop);
+        if (scrollTop > breakPoint) {
 
-            if ($position > $(document).scrollTop()) {
+            if ($position > scrollTop) {
+                if ($element.css("position") !== "absolute" && stickToTop === false) {
+                    $element.css("position", "absolute");
+                    $element.css("top", scrollTop - height);
+
+                } else {
+                    if (($position - scrollTop > 10  && stickToTop == false) ||
+                        (positionDifference - scrollTop >= height &&
+                        stickToTop == false)) {
+                        $element.css("position", "fixed");
+                        $element.css("top", 0);
+                        stickToTop = true;
+                    }
+                }
+                if (positionDifference == 0) {
+                    positionDifference = scrollTop;
+                }
 
             } else {
                 if ($element.css("position") !== "fixed") {
-                    $element.css("position", "fixed");
-                    $element.css("top", -(height));
+                    if (count <= 0 || scrollTop - $position > 10) {
+                        $element.css("position", "fixed");
+                        $element.css("top", -(height));
+                        positionDifference = 0;
+                        stickToTop = false;
+                    } else {
+                        count -= scrollTop - $position;
+                    }
+
+                } else {
+                    if (stickToTop) {
+                        $element.css("position", "absolute");
+                        $element.css("top", scrollTop);
+                        count = height;
+                    }
+
                 }
             }
         }
 
-        if ($(document).scrollTop() < breakPoint) {
-            $element.css("position", "absolute");
-            $element.css("top", 0);
+        if (scrollTop < breakPoint) {
+            if (!stickToTop) {
+                $element.css("position", "absolute");
+                $element.css("top", 0);
+            }
         }
 
-        $position = $(document).scrollTop();
+        if (scrollTop === 0) {
+            if (stickToTop) {
+                $element.css("position", "absolute");
+                $element.css("top", 0);
+            }
+        }
+
+        $position = scrollTop;
     })
 
 };
