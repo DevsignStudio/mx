9bf5be96f8408fe53beca00b7343032d337811cf
Nizul Zaim <shimizukazunari93@gmail.com>
2015-10-12 00:24:23 +0800

    Update Meteor and add Toolbar Title

---
+++ mobile-config.js
@@ -0,0 +1,48 @@
+App.info({
+  id: 'com.devsign.mx.materilex',
+  name: 'Materilex',
+  description: 'The Materilex template',
+  author: 'Devsign Studio',
+  email: 'skynightz93@gmail.com',
+  website: 'devsign.my'
+});
+
+App.icons({
+  // iOS
+  'iphone': 'resources/icons/iphone.icon.png',
+  'iphone_2x': 'resources/icons/iphone_2x.icon.png',
+  'iphone_3x': 'resources/icons/iphone_3x.icon.png',
+  'ipad': 'resources/icons/ipad.icon.png',
+  'ipad_2x': 'resources/icons/ipad_2x.icon.png',
+
+  // Android
+  'android_ldpi': 'resources/icons/android_ldpi.icon.png',
+  'android_mdpi': 'resources/icons/android_mdpi.icon.png',
+  'android_hdpi': 'resources/icons/android_hdpi.icon.png',
+  'android_xhdpi': 'resources/icons/android_xhdpi.icon.png'
+});
+
+App.launchScreens({
+  // iOS
+  'iphone': 'resources/splash/iphone.splash.png',
+  'iphone_2x': 'resources/splash/iphone_2x.splash.png',
+  'iphone5': 'resources/splash/iphone5.splash.png',
+  'iphone6': 'resources/splash/iphone6.splash.png',
+  'iphone6p_portrait': 'resources/splash/iphone6p_portrait.splash.png',
+  'iphone6p_landscape': 'resources/splash/iphone6p_landscape.splash.png',
+
+  'ipad_portrait': 'resources/splash/ipad_portrait.splash.png',
+  'ipad_portrait_2x': 'resources/splash/ipad_portrait_2x.splash.png',
+  'ipad_landscape': 'resources/splash/ipad_landscape.splash.png',
+  'ipad_landscape_2x': 'resources/splash/ipad_landscape_2x.splash.png',
+
+  // Android
+  'android_ldpi_portrait': 'resources/splash/android_ldpi_portrait.splash.png',
+  'android_ldpi_landscape': 'resources/splash/android_ldpi_landscape.splash.png',
+  'android_mdpi_portrait': 'resources/splash/android_mdpi_portrait.splash.png',
+  'android_mdpi_landscape': 'resources/splash/android_mdpi_landscape.splash.png',
+  'android_hdpi_portrait': 'resources/splash/android_hdpi_portrait.splash.png',
+  'android_hdpi_landscape': 'resources/splash/android_hdpi_landscape.splash.png',
+  'android_xhdpi_portrait': 'resources/splash/android_xhdpi_portrait.splash.png',
+  'android_xhdpi_landscape': 'resources/splash/android_xhdpi_landscape.splash.png'
+});
