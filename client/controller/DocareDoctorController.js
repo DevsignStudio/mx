57b6f412a80ede4eeb03f6c41deb753d7f40f1e6
nizulzaim <shimizukazunari93@gmail.com>
2015-10-16 20:01:34 +0800

    Fix duplicate controller settings

--- client/controller/DocareDoctorController.js
+++ client/controller/DocareDoctorController.js
@@ -1,8 +1,5 @@
 DocareDoctorController = BaseController.extend({
-    layoutTemplate:"ApplicationLayout",
-    notFoundTemplate:"404",
     yieldTemplates: {
        'pinkToolbar': {to: 'toolbar'},
-       'defaultNavigation': {to: "navigation"}
     }
 });
--- client/controller/DocareLoginController.js
+++ client/controller/DocareLoginController.js
@@ -1,8 +1,5 @@
 DocareLoginController = BaseController.extend({
-    layoutTemplate:"ApplicationLayout",
-    notFoundTemplate:"404",
     yieldTemplates: {
        'noToolbar': {to: 'toolbar'},
-       'defaultNavigation': {to: "navigation"}
     }
 });
