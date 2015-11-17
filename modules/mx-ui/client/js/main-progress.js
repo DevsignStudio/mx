cbe2c10b31cc050f079befce88fa41da71e1455e
nizulzaim <shimizukazunari93@gmail.com>
2015-11-17 20:23:18 +0800

    Add Progress Bar determinate

---
+++ modules/mx-ui/client/js/main-progress.js
@@ -0,0 +1,17 @@
+Mx.doAfterRouterLoad(function() {
+    $('.progress-bar.determinate').each(function() {
+        if (!$(this).isMeteorOnce('mx-progress-bar')) {
+            $(this).mxProgress();
+        }
+    });
+
+    $('[data-progress-add]').each(function() {
+
+        if (!$(this).isMeteorOnce('mx-progress-bar-add')) {
+            $(this).mxProgressAdd({
+                target: $(this).data("target"),
+                value: $(this).data("progress-add")
+            });
+        }
+    });
+});
---
+++ modules/mx-ui/client/js/mx-progress.js
@@ -0,0 +1,64 @@
+$.fn.mxProgress = function() {
+    $element = $(this);
+    var $selectMenu = $('<div class="accomplish"></div>');
+    var width = $element.data("current");
+
+    if ($element.hasAttr("data-goal")) {
+        var goal = $element.data("goal");
+        width = parseInt(width) / parseInt(goal);
+    }
+
+
+    $element.prepend($selectMenu);
+    $selectMenu.css({
+        "transform": "scaleX(" + width + ")",
+    });
+
+    $element.attrchange({
+        trackValues: true,
+        callback: function (event) {
+
+            if (event.attributeName === "data-current") {
+                width = event.newValue;
+                if ($element.hasAttr("data-goal")) {
+                    var goal = $element.data("goal");
+                    width = parseInt(width) / parseInt(goal);
+                }
+
+                $selectMenu.css({
+                    "transform": "scaleX(" + width + ")",
+                });
+            }
+        }
+    });
+};
+
+$.fn.mxProgressAdd = function(args) {
+    var Defaults = {
+        target: '',
+        value: "0",
+    };
+
+    args = $.extend(true, {}, Defaults, args);
+    var $element = $(this);
+    var $target = $(args.target);
+
+    $element.on("click", function() {
+        var currentWidth = $target.attr("data-current");
+        var width = parseInt(args.value);
+
+        if ($target.hasAttr("data-goal")) {
+            var goal = $target.data("goal");
+            width = parseInt(currentWidth) + width;
+            if (width > goal) {
+                width = goal;
+            }
+        } else {
+            if (width > 100) {
+                width = 100;
+            }
+        }
+
+        $target.attr("data-current", width);
+    });
+};
